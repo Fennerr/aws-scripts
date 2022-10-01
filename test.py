6326f410d5ef3935929d75b57bad166e014bbddb
powershell_user-at-415412769730 <jmoorcroft77@gmail.com>
2022-10-02 00:15:25 +0200

    Added awspx lib files and working debug in docker

---
+++ test.py
@@ -0,0 +1,7 @@
+# Import the utils library by modifying sys.path and then importing 'util'
+# import sys, pathlib
+# library_path = pathlib.Path(__file__).absolute().parent.parent / 'lib'
+# sys.path.append( str(library_path))
+import lib.util
+
+print('done')
